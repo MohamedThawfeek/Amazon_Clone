73627fc0bcab02f1599593bc7ed7946d68ebd204
Mohamed Thawfeek <89966428+MohamedThawfeek@users.noreply.github.com>
2022-03-30 21:11:16 +0530

    Add files via upload

---
+++ amazon_frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminPanel from "./allPages/AdminPanel";
+import User from "./allPages/Users";
+
+const App = () => {
+  return (
+    <div>
+      <AdminPanel />
+      <User />
+    </div>
+  );
+};
+
+export default App;
