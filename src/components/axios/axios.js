961a89605798d33c7b52916f5ee42aef6172545b
Mohamed Thawfeek <89966428+MohamedThawfeek@users.noreply.github.com>
2022-03-30 21:10:16 +0530

    Delete src directory

--- src/components/axios/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://my-first-mern-app-a-c.herokuapp.com",
-});
-
-export default instance;
--- src/pages/admin dashboard pages/globalorders/GlobalOrders.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GlobalOrders = () => {
-  return <div>GlobalOrders</div>;
-};
-
-export default GlobalOrders;
